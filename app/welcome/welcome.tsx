3a6319ed687244c150610aca5b96f5d483678eb5
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-05 09:39:55 +0100

    Created increment and decrement buttons

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,16 +3,18 @@ import {useState} from "react";
 export function Welcome() {
 
     const [clicks, setClicks] = useState(0);
-    function onClick() {
-        setClicks(clicks + 1);
-    }
+    const increment = () => setClicks(clicks + 1);
+    const decrement = () => {
+        if (clicks > 0) setClicks(clicks - 1)
+    };
 
     return (
         <main className="flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
                 <h1>Hello!</h1>
                 <p>Count: {clicks}</p>
-                <button onClick={onClick}> Increase count </button>
+                <button onClick={increment} className={"border-2 border-amber-200"}>Increase count</button>
+                <button onClick={decrement} className={"border-2 border-cyan-300"}>Decrease count</button>
             </div>
         </main>
     );
