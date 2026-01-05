dfbb2feb85892f44b701bf01d66a8504e5c534ef
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-05 10:06:13 +0100

    Added hover for dark style

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -8,6 +8,7 @@ export default function Button(props: Props) {
         <>
             <button onClick={props.onClick} className={"border-2 " +
                 "dark:border-pink-400 dark:bg-pink-300 dark:text-black " +
+                "dark:hover:border-pink-500 dark:hover:bg-pink-400 dark:text-black " +
                 "p-3 rounded-sm"}
             >{props.name}</button>
         </>
