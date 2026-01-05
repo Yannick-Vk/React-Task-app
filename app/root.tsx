03ba5c06b32c80065be918e10efb399d268e7328
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-05 09:32:49 +0100

    Added title

--- app/root.tsx
+++ app/root.tsx
@@ -20,6 +20,7 @@ export function Layout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <head>
+            <title>React ToDo</title>
             <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <Meta/>
