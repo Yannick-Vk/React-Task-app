6aae2803f1cc99fda059900eeb4b3cc9274e3dff
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-05 10:19:56 +0100

    className cleanup

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,8 +7,8 @@ export default function Button(props: Props) {
     return (
         <>
             <button onClick={props.onClick} className={"p-3 rounded-sm " +
-                "dark:border-pink-400 dark:bg-pink-300 dark:text-black " +
-                "dark:hover:border-pink-500 dark:hover:bg-pink-400 dark:text-black "
+                " dark:bg-pink-300 dark:text-black " +
+                " dark:hover:bg-pink-400 "
             }
             >{props.name}</button>
         </>
