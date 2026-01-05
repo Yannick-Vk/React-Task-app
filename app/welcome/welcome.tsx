98b8ecf3e4bd18c4fbcf1a9408f5d6a857885f3f
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-05 09:31:59 +0100

    Created simple counter

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,8 +1,18 @@
+import {useState} from "react";
+
 export function Welcome() {
+
+    const [clicks, setClicks] = useState(0);
+    function onClick() {
+        setClicks(clicks + 1);
+    }
+
     return (
         <main className="flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
                 <h1>Hello!</h1>
+                <p>Count: {clicks}</p>
+                <button onClick={onClick}> Increase count </button>
             </div>
         </main>
     );
