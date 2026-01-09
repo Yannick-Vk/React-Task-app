6e935df4b8ed888531a2c5746c9b355e9319fa3d
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-09 14:18:35 +0100

    Added example without a status, removed specifaction of Task type when using new keyword

--- GraphQLTasks/Types/Query.cs
+++ GraphQLTasks/Types/Query.cs
@@ -2,12 +2,12 @@ namespace GraphQLTasks.Types;
 
 [QueryType]
 public static class Query {
-    public static Task GetTask()
-        => new Task("Feed my cats!", Status.Done);
+    public static Task GetTask() => new("Feed my cats!", Status.Done);
 
     public static List<Task> GetTasks() => [
-        new Task("Feed my cats!", Status.Done), 
-        new Task("Do homework!", Status.InProgress), 
-        new Task("Try to behave!", Status.UnderReview), 
+        new("Prepare food"),
+        new("Feed my cats!", Status.Done), 
+        new("Do homework!", Status.InProgress), 
+        new("Try to behave!", Status.UnderReview), 
     ];
 }
