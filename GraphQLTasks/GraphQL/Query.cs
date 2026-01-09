235aee8263a6c075d5e5a203939944c66b6fbab1
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-09 15:06:07 +0100

    Removed warning for query never instantiated

--- GraphQLTasks/GraphQL/Query.cs
+++ GraphQLTasks/GraphQL/Query.cs
@@ -3,6 +3,7 @@ using Task = GraphQLTasks.Models.Task;
 
 namespace GraphQLTasks.GraphQL;
 
+// ReSharper disable once ClassNeverInstantiated.Global
 public class Query {
     public IQueryable<Task> GetTasks([Service] ApplicationDbContext context) {
         return context.Tasks;
