bf4b388076038e201421a45bf6911c661249d993
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-09 15:13:29 +0100

    Updated to use service via DI

--- GraphQLTasks/GraphQL/Mutation.cs
+++ GraphQLTasks/GraphQL/Mutation.cs
@@ -7,16 +7,26 @@ namespace GraphQLTasks.GraphQL;
 // ReSharper disable once ClassNeverInstantiated.Global
 // Disabled since setting to abstract breaks the query
 public class Mutation {
-    public async Task<Task> AddTask(string name, Status status, [Service] ApplicationDbContext context) {
+    public Mutation(ApplicationDbContext context) {
+        _context = context;
+    }
+
+    private ApplicationDbContext _context { get; }
+
+    public async Task<Task> AddTask(string name, Status status) {
         var task = new Task {
             Id = Guid.NewGuid(),
             Name = name,
             Status = status,
         };
 
-        context.Tasks.Add(task);
-        await context.SaveChangesAsync();
+        _context.Tasks.Add(task);
+        await _context.SaveChangesAsync();
 
         return task;
+    }
+
+    public async Task<Task?> RemoveTask(Guid id) {
+        return null;
     }
 }
--- GraphQLTasks/GraphQL/Query.cs
+++ GraphQLTasks/GraphQL/Query.cs
@@ -5,7 +5,13 @@ namespace GraphQLTasks.GraphQL;
 
 // ReSharper disable once ClassNeverInstantiated.Global
 public class Query {
-    public IQueryable<Task> GetTasks([Service] ApplicationDbContext context) {
-        return context.Tasks;
+    public Query(ApplicationDbContext context) {
+        _context = context;
+    }
+
+    private ApplicationDbContext _context { get; }
+
+    public IQueryable<Task> GetTasks() {
+        return _context.Tasks;
     }
 }
