2da0f812ab7fedaf07008fcd1377db78fe3e0b56
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-10 12:13:32 +0100

    Added maximum string length for name

---
+++ GraphQLTasks/Migrations/20260110111048_added_max_length_for_name.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphQLTasks.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_max_length_for_name : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tasks",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tasks",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
--- GraphQLTasks/Models/Task.cs
+++ GraphQLTasks/Models/Task.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GraphQLTasks.Models;
 
 public class Task {
     public Guid Id { get; set; }
-    public required string Name { get; set; }
+
+    [StringLength(255)] public required string Name { get; set; }
+
     public Status Status { get; set; } = Status.Ready;
 }
 
