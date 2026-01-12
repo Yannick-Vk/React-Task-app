90ef68a3b2b17abb54a2383d31eddd784123232a
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-12 13:02:57 +0100

    Updated button text

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         <main className="w-11/12 m-auto flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
                 <h1>Task board</h1>
-                <Button name={"Open task creator"} onClick={openModal} /> {/* Button to open modal */}
+                <Button name={"Create a new task"} onClick={openModal} /> {/* Button to open modal */}
                 <Modal title="Create a new task" isOpen={isModalOpen} onClose={closeModal}>
                     <div className="flex flex-col gap-3">
                         <CreateTask createNewTask={addNewTask} />
