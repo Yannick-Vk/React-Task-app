bcb8d7199720e566267799134de63b11c68e181c
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-12 10:40:07 +0100

    Created a basic modal

---
+++ app/components/Modal.tsx
@@ -0,0 +1,20 @@
+﻿import React, {Children} from "react";
+import Button from "~/components/Button";
+
+export interface Props {
+    className?: string;
+    children: React.ReactNode;
+}
+
+export default function Modal(props: Props) {
+
+    const [state, setState] = React.useState(false)
+
+    return (
+        <>
+            <Button name={"Open modal"} onClick={() => setState(!state)}></Button>
+            <span>{state}</span>
+            {Children}
+        </>
+    );
+}
