0bf288bef3e687e6e61139c33112418d5ac2ae7c
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-12 16:11:13 +0100

    Added port to link

--- app/lib/apollo.ts
+++ app/lib/apollo.ts
@@ -2,6 +2,6 @@ import {ApolloClient, HttpLink, InMemoryCache} from "@apollo/client";
 
 export const client = new ApolloClient({
     // Replace this with your GraphQL API endpoint
-    link: new HttpLink({uri: "http://localhost/graphql"}),
+    link: new HttpLink({uri: "http://localhost:5095/graphql"}),
     cache: new InMemoryCache(),
 });
