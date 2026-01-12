8c35d6ef45641fea07448d7c3607ad0cac378284
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-12 13:02:43 +0100

    Moved logic out of a function

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -11,36 +11,28 @@ export interface Props {
 
 export default function Modal(props: Props) {
 
-    const showContent = () => {
-        if (!props.isOpen) {
-            return null;
-        }
+    return (
+        <>
+            {!props.isOpen ? null : (
+                <div className="fixed inset-0 z-50 overflow-y-auto">
+                    {/* Background overlay */}
+                    <div className="fixed inset-0 bg-zinc-800 opacity-80" aria-hidden="true"></div>
 
-        return (
-            <div className="fixed inset-0 z-50 overflow-y-auto">
-                {/* Background overlay */}
-                <div className="fixed inset-0 bg-zinc-800 opacity-80" aria-hidden="true"></div>
-
-                {/* Modal content container */}
-                <div className="flex items-center justify-center min-h-screen">
-                    <div className="bg-slate-800 p-5 rounded-lg shadow-lg relative z-50">
-                        <div className="flex items-center gap-5 justify-between mb-3">
-                            <span className="font-bold text-lg">{props.title}</span>
-                            <ButtonRaw onClick={props.onClose}
-                                       className={"rounded-sm hover:text-fuchsia-200 hover:cursor-pointer"}>X</ButtonRaw>
-                        </div>
-                        <div className={props.className}>
-                            {props.children}
+                    {/* Modal content container */}
+                    <div className="flex items-center justify-center min-h-screen">
+                        <div className="bg-slate-800 p-5 rounded-lg shadow-lg relative z-50">
+                            <div className="flex items-center gap-5 justify-between mb-3">
+                                <span className="font-bold text-lg">{props.title}</span>
+                                <ButtonRaw onClick={props.onClose}
+                                           className={"rounded-sm hover:text-fuchsia-200 hover:cursor-pointer"}>X</ButtonRaw>
+                            </div>
+                            <div className={props.className}>
+                                {props.children}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        )
-    }
-
-    return (
-        <>
-            {showContent()}
+            )}
         </>
     );
 }
