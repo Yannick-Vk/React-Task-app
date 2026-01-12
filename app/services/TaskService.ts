bd75b21b452ff33b20c15af72ced1f06dc723b10
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-12 15:54:51 +0100

    Updated type of ID and added console log to see error

--- app/services/TaskService.ts
+++ app/services/TaskService.ts
@@ -3,7 +3,7 @@ import {z, ZodError} from "zod";
 import {client} from "~/lib/apollo";
 
 const TaskSchema = z.object({
-    id: z.number().int().positive(),
+    id: z.string(),
     name: z.string().min(1, "Task name cannot be empty"),
     status: z.enum(Status),
 });
@@ -13,10 +13,8 @@ type AddTaskResult = { success: true; data: Task[] } | { success: false; error: 
 
 // Add a new task to the list, returns the list or null when the new task was invalid
 export const addNewTask = (tasks: Task[], taskName?: string, status?: Status): AddTaskResult => {
-
-    const maxId = tasks.length ? Math.max(...tasks.map(p => p.id)) : 0;
     const newTaskDraft = {
-        id: maxId + 1,
+        id: 0,
         name: taskName?.toString().trim(),
         status: status ?? Status.Ready,
     };
@@ -31,15 +29,15 @@ export const addNewTask = (tasks: Task[], taskName?: string, status?: Status): A
 }
 
 // Remove a task by its id
-export const removeTask = (tasks: Task[], id: number): Task[] => {
+export const removeTask = (tasks: Task[], id: string): Task[] => {
     return tasks.filter((task) => task.id !== id);
 }
 
 // Change a given task's status
-export const changeStatus = (tasks: Task[], id: number, status: string): Task[] => {
+export const changeStatus = (tasks: Task[], id: string, status: Status): Task[] => {
     return tasks.map(task => {
         if (task.id === id) {
-            return {...task, status: status as Status};
+            return {...task, status: status};
         }
         return task;
     })
@@ -51,6 +49,8 @@ export const getTasks = async (): Promise<Task[]> => {
         const {data} = await client.query<GetTasksQuery>({
             query: GetTasksDocument,
         });
+        // TODO: Fix Cors Error
+        console.log("Fetched tasks from GraphQL:", data?.tasks);
         return data?.tasks || [];
     } catch (error) {
         console.error("Error fetching tasks:", error);
