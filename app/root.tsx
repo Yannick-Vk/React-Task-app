c0f6460145bd8b618fc689bf9539182b55fc4f23
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-12 10:40:36 +0100

    Added react import

--- app/root.tsx
+++ app/root.tsx
@@ -2,6 +2,7 @@ import {isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration,} 
 
 import type {Route} from "./+types/root";
 import "./app.css";
+import React from "react";
 
 export const links: Route.LinksFunction = () => [
     {rel: "preconnect", href: "https://fonts.googleapis.com"},
