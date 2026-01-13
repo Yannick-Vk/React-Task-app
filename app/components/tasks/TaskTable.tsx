12e7e7eba1cd13b4fcc09277356cebbe97390b79
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-13 16:31:57 +0100

    Removed task-id from table

--- app/components/tasks/TaskTable.tsx
+++ app/components/tasks/TaskTable.tsx
@@ -19,14 +19,12 @@ export default function TaskTable(props: Props) {
     return (
         <>
             <Table columns={[
-                {key: "id", header: "Id"},
                 {key: "title", header: "Title"},
                 {key: "status", header: "Status"},
                 {key: "actions", header: "Actions"},
             ]}>
                 {props.data.map((item) => (
                     <tr className={"bg-gray-200 even:bg-gray-300 text-black hover:bg-pink-200"} key={item.id}>
-                        <td className={"p-3 text-center"}> {item.id} </td>
                         <td className={"p-3 text-center"}> {item.name} </td>
                         <td className={"p-3 text-center"}>
                             <TaskTypesSelectBox name="" value={item.status} className={"bg-slate-200"}
