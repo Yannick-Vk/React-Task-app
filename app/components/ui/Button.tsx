02c9c1eabea04562fa7706db094b8b9f33369a81
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-15 23:30:53 +0100

    Added disabled styling and switched to twMerge

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,25 +1,26 @@
 ﻿import React from "react";
 import ButtonRaw from "~/components/ui/raw/ButtonRaw";
+import {twMerge} from "tailwind-merge";
 
 export interface Props {
     onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
     className?: string;
     type?: "button" | "submit" | "reset";
     disabled?: boolean;
-    children?: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export default function Button(props: Props) {
     return (
         <>
             <ButtonRaw disabled={props.disabled} onClick={props.onClick}
-                       className={(props.className ?? "") + " p-3 rounded-sm " +
-                           " dark:bg-pink-300 dark:text-black " +
-                           " dark:hover:bg-pink-400 "
+                       className={twMerge("p-3 rounded-sm",
+                           "dark:bg-pink-300 dark:text-black dark:hover:bg-pink-400 disabled:cursor-not-allowed disabled:bg-gray-400",
+                           props.className)
                        }
                        type={props.type ?? "button"}
             >
-                {props.children ?? ""}
+                {props.children}
             </ButtonRaw>
         </>
     )
