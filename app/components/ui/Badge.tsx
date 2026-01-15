8dca48beaf05ab522a307e1cb37ce434c1bf37c8
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-15 16:27:37 +0100

    Implemented badge with variants and mapped priority into variants

--- app/components/ui/Badge.tsx
+++ app/components/ui/Badge.tsx
@@ -1,16 +1,37 @@
 ﻿import {twMerge} from "tailwind-merge";
+import React from "react";
 
 export interface Props {
     className?: string;
-    variant?: "gray" | "error" | "warning";
+    variant?: BadgeVariant;
+    children: React.ReactNode;
+}
+
+export enum BadgeVariant {
+    gray, error, warning
 }
 
 export default function Badge(props: Props) {
-    return (
-        <>
-            <div className={twMerge("", props.className)}>
 
-            </div>
-        </>
+    let variant = "";
+    switch (props.variant) {
+        case BadgeVariant.gray:
+            variant = "bg-zinc-500 text-white";
+            break;
+        case BadgeVariant.error:
+            variant = "bg-red-500 text-white";
+            break;
+        case BadgeVariant.warning:
+            variant = "bg-amber-500 text-white";
+            break;
+        default:
+            variant = "bg-gray-500 text-white";
+            break;
+    }
+
+    return (
+        <div className={twMerge("px-2 py-1 rounded-md text-sm", variant, props.className)}>
+            {props.children}
+        </div>
     );
 }
