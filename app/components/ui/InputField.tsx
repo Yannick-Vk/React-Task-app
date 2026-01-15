4b13b200d091ae8ae5702cdcc9cb4aaf4e7ab7e2
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-15 22:48:27 +0100

    Switched from string to Input type and added default type

--- app/components/ui/InputField.tsx
+++ app/components/ui/InputField.tsx
@@ -1,4 +1,4 @@
-﻿import React from "react";
+﻿import React, {type HTMLInputTypeAttribute} from "react";
 import {twMerge} from "tailwind-merge";
 import FormField from "~/components/ui/FormField";
 
@@ -6,7 +6,7 @@ export interface Props {
     className?: string;
     label: string;
     name: string;
-    type?: string;
+    type?: HTMLInputTypeAttribute;
     value: string;
     error: string[] | undefined;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -15,8 +15,9 @@ export interface Props {
 export default function InputField(props: Props) {
     return (
         <FormField label={props.label} name={props.name} error={props.error}>
-            <input value={props.value} type={props.type} name={props.name} id={props.name} onChange={props.onChange}
-                   className={twMerge("rounded-sm border-2 border-slate-300 text-gray-300 p-3 w-full focus:outline-none focus:shadow-outline focus:border-pink-300",
+            <input value={props.value} type={props.type ?? "text"} name={props.name} id={props.name}
+                   onChange={props.onChange}
+                   className={twMerge("rounded-sm border-2 border-slate-300 text-gray-300 p-3 w-full focus:outline-none focus:shadow-outline focus:border-pink-300 hover:border-pink-300",
                        props.className)} />
         </FormField>
     );
