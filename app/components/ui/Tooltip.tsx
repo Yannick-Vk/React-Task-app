a37ef667c39006f8db5bdc5235db06e983a11a6a
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-15 16:51:59 +0100

    Centered tooltip

--- app/components/ui/Tooltip.tsx
+++ app/components/ui/Tooltip.tsx
@@ -15,7 +15,9 @@ export default function Tooltip(props: Props) {
             <div
                 className={twMerge(
                     "absolute bottom-full mb-2 hidden group-hover:block",
+                    "left-1/2 -translate-x-1/2 transform w-max",
                     "bg-gray-800 text-white text-xs rounded-md px-2 py-1",
+                    "pointer-events-none",
                     props.className
                 )}>
                 {props.content}
