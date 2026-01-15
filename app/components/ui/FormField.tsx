891d7d7a88d8cbc2054cdbfe30cb41d32ac03877
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-15 21:06:29 +0100

    Created generic FormField Component

---
+++ app/components/ui/FormField.tsx
@@ -0,0 +1,23 @@
+﻿import React from "react";
+
+export interface Props {
+    className?: string;
+    label: string;
+    name: string;
+    error: string[] | undefined;
+    children: React.ReactNode;
+}
+
+export default function FormField(props: Props) {
+    return (
+        <div className={props.className}>
+            <label htmlFor={props.name}
+                   className={"block font-medium text-gray-300 mb-2"}>{props.label}</label>
+            {props.children}
+            {props.error?.map((err) => (
+                    <span className="text-red-500 text-sm">{err}</span>
+                )
+            )}
+        </div>
+    );
+}
