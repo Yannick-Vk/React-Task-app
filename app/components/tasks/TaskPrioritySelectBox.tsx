48214e202bc9ad0dbd5561bc199d2b8bcf714ff7
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-15 20:06:08 +0100

    Created a Priority SelectBox

---
+++ app/components/tasks/TaskPrioritySelectBox.tsx
@@ -0,0 +1,34 @@
+﻿import {Priority, Status} from "~/GraphQL/generated"
+import React from "react";
+import EnumSelectBox from "~/components/ui/EnumSelectBox";
+
+export interface Props {
+    name: string;
+    className?: string;
+    value?: Status; // controlled value (optional)
+    onChange?: (value: Status) => void;
+}
+
+export function MapEnum(value: Priority): string {
+    switch (value) {
+        case Priority.Low:
+            return "Low";
+        case Priority.Medium:
+            return "Medium";
+        case Priority.High:
+            return "High";
+        case Priority.None:
+            return "None";
+        default:
+            return value;
+    }
+}
+
+export default function TaskPrioritySelectBox(props: Props) {
+    return (
+        <>
+            <EnumSelectBox name={props.name} enum={Priority} mapEnumToLabel={MapEnum}
+                           className={props.className}></EnumSelectBox>
+        </>
+    );
+}
