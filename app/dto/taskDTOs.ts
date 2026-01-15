f346144fcda08ed3bee045c25a744340a2e48bfa
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-15 15:26:07 +0100

    Created a taskDto's file and added a UpdateTaskDTO interface

---
+++ app/dto/taskDTOs.ts
@@ -0,0 +1,10 @@
+﻿import {Priority, Status} from "~/GraphQL/generated";
+import type {Option} from "~/lib/util";
+
+export interface UpdateTaskDTO {
+    name: string,
+    status: Status,
+    description: Option<string>,
+    priority: Option<Priority>,
+    dueDate: Option<Date>
+}
