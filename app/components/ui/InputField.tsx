4fa0b14eaf89919d7b3ceb5b7b1b16d02290cbfe
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-15 21:06:43 +0100

    Moved to use new Generic FormField component

--- app/components/ui/InputField.tsx
+++ app/components/ui/InputField.tsx
@@ -1,5 +1,6 @@
 ﻿import React from "react";
 import {twMerge} from "tailwind-merge";
+import FormField from "~/components/ui/FormField";
 
 export interface Props {
     className?: string;
@@ -13,14 +14,10 @@ export interface Props {
 
 export default function InputField(props: Props) {
     return (
-        <>
-            <label htmlFor={props.name} className="block font-medium text-gray-300 mb-2">{props.label}</label>
+        <FormField label={props.label} name={props.name} error={props.error}>
             <input value={props.value} type={props.type} name={props.name} id={props.name} onChange={props.onChange}
                    className={twMerge("rounded-sm border-2 border-slate-300 text-gray-300 p-3 w-full focus:outline-none focus:shadow-outline focus:border-pink-300",
                        props.className)} />
-            {props.error?.map((err) => (
-                <span className="text-red-500 text-sm">{err}</span>
-            ))}
-        </>
+        </FormField>
     );
 }
