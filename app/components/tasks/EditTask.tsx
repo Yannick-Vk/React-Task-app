adb9e04de7dde5830636e80fa3c8c3cc79e85e5c
Yannick <vankerckvoordeyannick@gmail.com>
2026-01-16 15:03:57 +0100

    Load selectedTask state into Modal, added reset functionality

--- app/components/tasks/EditTask.tsx
+++ app/components/tasks/EditTask.tsx
@@ -7,7 +7,7 @@ import TextareaField from "~/components/ui/TextareaField";
 import TaskPrioritySelectBox from "~/components/tasks/TaskPrioritySelectBox";
 import AlertBox from "~/components/ui/AlertBox";
 import {useStateWithReset} from "~/hooks/useStateWithReset";
-import type {DateTime} from "luxon";
+import {DateTime} from "luxon";
 
 export interface Props {
     className?: string;
@@ -33,23 +33,36 @@ type FormErrors = {
 
 export default function EditTask(props: Props) {
     // Form data
-    const [name, setName] = useState("");
-    const [status, setStatus] = useState<Status>(Status.Ready);
-    const [priority, setPriority] = useState<Priority>(Priority.None);
-    const [dueDate, setDueDate] = useState<DateTime | undefined>(undefined);
-    const [description, setDescription] = useState<string>("");
+    const [name, setName] = useState(props.selectedTask?.name);
+    const [status, setStatus] = useState(props.selectedTask?.status);
+    const [priority, setPriority] = useState(props.selectedTask?.priority);
+    const [dueDate, setDueDate] = useState<DateTime | undefined>(props.selectedTask?.dueDate ? DateTime.fromISO(props.selectedTask.dueDate) : undefined);
+    const [description, setDescription] = useState(props.selectedTask?.description);
     // Generic
     const [errors, setErrors, resetError] = useStateWithReset<FormErrors | null>(null);
     const [genericError, setGenericError, resetGenericError] = useStateWithReset<string | null>(null);
     const [isLoading, setIsLoading] = useState(false);
+    const [originalTask, setOriginalTask] = useState(props.selectedTask);
 
-    const handleNameChange = () => {}
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+    }
 
-    const handleDescriptionChange = () => {}
+    const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setDescription(e.target.value);
+    }
 
-    const handleDueDateChange = () => {}
+    const handleDueDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDueDate(e.target.value ? DateTime.fromISO(e.target.value) : undefined);
+    }
 
-    const reset = (): void => {}
+    const reset = (): void => {
+        setName(originalTask?.name);
+        setStatus(originalTask?.status);
+        setDueDate(originalTask?.dueDate);
+        setPriority(originalTask?.priority);
+        setDescription(originalTask?.description);
+    }
 
     const updateTask = () => {}
 
@@ -57,19 +70,20 @@ export default function EditTask(props: Props) {
         <div className="flex flex-col gap-3">
             <p>Update the task, click 'esc' or the close button to cancel. Press reset to go back to the
                 original state.</p>
-            <InputField label="Task Name" name="name" value={name} onChange={handleNameChange}
+            <InputField label="Task Name" name="name" value={name ?? ""} onChange={handleNameChange}
                         error={errors?.name} required={true} />
 
-            <TextareaField label="Description" name="description" value={description}
+            <TextareaField label="Description" name="description" value={description ?? ""}
                            error={errors?.description}
                            onChange={handleDescriptionChange} maxCharacters={500} />
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <TaskStatusSelectBox label="Status" className={"focus:border-pink-300 bg-slate-800"}
-                                     name="status" value={status} error={errors?.status} onChange={setStatus} />
+                                     name="status" value={status ?? Status.Ready} error={errors?.status}
+                                     onChange={setStatus} />
 
                 <TaskPrioritySelectBox label="Priority" className={"focus:border-pink-300 bg-slate-800"}
-                                       name="priority" value={priority} error={errors?.priority}
+                                       name="priority" value={priority ?? Priority.None} error={errors?.priority}
                                        onChange={setPriority} />
             </div>
 
